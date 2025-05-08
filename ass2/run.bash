e9a80c1bff2f58d06c5facb04cb0fb9b3a0881e6
Jordy <jdelcastilho@tudelft.nl>
2025-05-08 18:22:22 +0200

    part 4: add ssh keys to vms

--- ass2/run.bash
+++ ass2/run.bash
@@ -1,22 +1,13 @@
+#!/bin/bash
+echo "[*] Copying SSH keypair into WSL home..."
+
+mkdir -p ~/vagrant_ssh
+cp ./ssh/id_rsa ~/vagrant_ssh/vagrant_id_rsa
+cp ./ssh/id_rsa.pub ~/vagrant_ssh/vagrant_id_rsa.pub
+
+chmod 600 ~/vagrant_ssh/vagrant_id_rsa
+chmod 644 ~/vagrant_ssh/vagrant_id_rsa.pub
+
+echo "[+] SSH key ready in ~/vagrant_ssh/vagrant_id_rsa"
 
 vagrant up
-
-# amount_of_nodes=$(grep -E '^\s*num_of_workers\s*=' Vagrantfile | awk -F= '{print $2}' | tr -d ' \r')
-# echo "Number of worker nodes: $amount_of_nodes":contentReference[oaicite:4]{index=4}
-
-# echo "Amount of nodes: $amount_of_nodes"
-
-# #for amount of nodes
-# for i in $(seq 0 "$amount_of_nodes"); do
-#     ip="192.168.56.$((100 + i))"
-#     echo "Copying ssh key to $ip"
-#     ssh-copy-id -o StrictHostKeyChecking=no vagrant@"$ip"
-# done
-
-echo "Pinging nodes"
-ansible ctrl -i inventory.cfg -m ping
-
-
-
-
-
